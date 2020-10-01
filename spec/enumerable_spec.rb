722bef861dd020bdd8458d059a20ad839523a5de
alvaro <aaruizf@unal.edu.co>
2020-10-01 09:23:37 -0500

    Create tests structure

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,90 @@
+# spec/game_logic_spec.rb
+require_relative '../enumerable.rb'
+
+describe Enumerable do
+  describe '#my_each' do
+    it 'If a block is not given it should return the enumerable' do
+    end
+
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it '' do
+    end
+
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_select' do
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_all?' do
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_any?' do
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_none?' do
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_count' do
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_map' do
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#my_inject' do
+    it '' do
+    end
+
+    it '' do
+    end
+
+    it '' do
+    end
+  end
+
+  describe '#multiply_els' do
+    it '' do
+    end
+  end
+end
